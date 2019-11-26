84eec2bfd9635ad8786dbbd7e0f350faed183384
Hipp0Shok <gleb.zhivotov@mail.ru>
2019-11-26 03:16:31 +0300

    Fixed EOF

--- count.c
+++ count.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
-void main(void)  /*  COUNT.C  */
+#include <stdlib.h>
+int main(void)  /*  COUNT.C  */
             /*  Процесс, выполняющий  COUNT,  должен    */
             /*  переназначить ввод и вывод так,    */
 /*  чтобы ввод данных выполнялся из канала P,     */
 /*  а вывод – в канал Q    */
             /*        */
-//Код не рабоатет верно с кириллицей. Используйте пока латиницу
+/*Код не рабоатет верно с кириллицей. Используйте пока латиницу */
 {
     int count = 0;
-    while (getchar() != EOF)
+    int c;
+    while (1)
     {
+    	c = getchar();
+    	{
+    		if(c == EOF)
+    		{
+    			if(feof(stdin))
+    			{
+    				break;
+    			}
+    			else
+    			{
+    				perror("Error while trying to get char");
+    				exit(1);
+    			}
+    		}
+    	}
     	count++;	
     }
 	printf("%d\n", count);
